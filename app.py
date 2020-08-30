da7757d7b63c06cb2905834ff90e626ce4b87436
mjtoolbox <michael.jo@gmail.com>
2020-08-30 16:16:36 -0700

    initialized

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+import sqlite3
+import sys
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("welcome.html")
+
+@app.route("/journals")
+def journals():
+    return render_template("Journals.html")
+
+@app.route("/new")
+def newJournal():
+    return render_template("NewJournal.html")
